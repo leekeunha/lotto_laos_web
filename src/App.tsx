f1976347facdde7ce88c7798188a5e0f80df29c4
leekeunha <leetoya@naver.com>
2024-05-12 19:28:22 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import { Button } from '@material-tailwind/react';
+
+function App() {
+  const [count, setCount] = useState('');
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
+  return <Button onClick={handleClick}>{count}</Button>;
+}
+
+export default App;
