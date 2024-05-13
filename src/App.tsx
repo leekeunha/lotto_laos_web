796630983f1bc3e2a5f0b2a7e78054a845af00bf
leekeunha <leetoya@naver.com>
2024-05-13 14:54:39 +0900

    const [count, setCount] = useState(0);

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,11 @@ import React, { useState } from 'react';
 import { Button } from '@material-tailwind/react';
 
 function App() {
-  const [count, setCount] = useState('');
+  const [count, setCount] = useState(0);
 
   const handleClick = () => {
     setCount(count + 1);
   };
-
   return <Button onClick={handleClick}>{count}</Button>;
 }
 
