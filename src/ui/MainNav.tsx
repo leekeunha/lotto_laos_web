91f0f52d5cdafd5b33ff55cc1ad65c42e5dd2a98
leekeunha <leetoya@naver.com>
2024-05-20 19:12:09 +0900

    before home

--- src/ui/MainNav.tsx
+++ src/ui/MainNav.tsx
@@ -246,19 +246,3 @@ export default function MainNav() {
         </nav>
     );
 }
-// import NavItem from './NavItem';
-
-// export default function MainNav() {
-//     return (
-//         <nav>
-//             <ul className="mb-4 mt-4 flex flex-row items-center gap-8">
-//                 <NavItem label="Lottery information" to="/lottery-information" />
-//                 <NavItem label="Result" to="/result" />
-//                 <NavItem label="Gift Game" to="/gift-game" />
-//                 <NavItem label="Store" to="store" />
-//                 <NavItem label="About DLPE" to="about-dlpe" />
-//                 <NavItem label="Service center" to="service-center" />
-//             </ul>
-//         </nav>
-//     );
-// }
