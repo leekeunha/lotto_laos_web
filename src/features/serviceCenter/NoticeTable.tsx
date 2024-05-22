85ef7680815edd1422323c941e5758fb5f6839ba
leekeunha <leetoya@naver.com>
2024-05-22 18:06:34 +0900

    bug fix

--- src/features/serviceCenter/NoticeTable.tsx
+++ src/features/serviceCenter/NoticeTable.tsx
@@ -8,6 +8,7 @@ import {
 } from '@material-tailwind/react';
 import { useQuery } from '@tanstack/react-query';
 import { useServiceCenterServiceStore } from '../../store/serviceCenterStore';
+import { TABLE_HEAD } from '../../constants';
 
 export default function NoticeTable({ keyword }) {
     const { serviceCenterService } = useServiceCenterServiceStore();
@@ -25,100 +26,68 @@ export default function NoticeTable({ keyword }) {
         <>
             {isLoading && <p>Loading...</p>}
             {error && <p>Something is wrong 😖</p>}
-            <CardBody className="overflow-scroll px-0">
-                <table className="w-full min-w-max table-auto text-left">
-                    {/* <thead>
-                        <tr>
-                            {TABLE_HEAD.map((head) => (
-                                <th
-                                    key={head}
-                                    className="border-y border-blue-gray-100 bg-blue-gray-50/50 p-4"
-                                >
-                                    <Typography
-                                        variant="small"
-                                        color="blue-gray"
-                                        className="font-normal leading-none opacity-70"
-                                    >
-                                        {head}
-                                    </Typography>
-                                </th>
-                            ))}
-                        </tr>
-                    </thead> */}
-                    <tbody>
-                        {notices &&
-                            notices.map(({ no, title, date }, index) => {
-                                const isLast = index === notices.length - 1;
-                                const classes = isLast ? 'p-4' : 'p-4 border-b border-blue-gray-50';
 
-                                return (
-                                    <tr key={no}>
-                                        <td className={classes}>
-                                            <div className="flex items-center gap-3">
-                                                <Typography
-                                                    variant="small"
-                                                    color="blue-gray"
-                                                    className="font-bold"
-                                                >
-                                                    {no}
-                                                </Typography>
-                                            </div>
-                                        </td>
-                                        <td className={classes}>
+            <table className="w-full min-w-max table-auto text-left">
+                <thead>
+                    <tr>
+                        {TABLE_HEAD.map((head) => (
+                            <th
+                                key={head}
+                                className="border-y border-blue-gray-100 bg-blue-gray-50/50 p-4"
+                            >
+                                <Typography
+                                    variant="small"
+                                    color="blue-gray"
+                                    className="font-normal leading-none opacity-70"
+                                >
+                                    {head}
+                                </Typography>
+                            </th>
+                        ))}
+                    </tr>
+                </thead>
+                <tbody>
+                    {notices &&
+                        notices.map(({ no, title, date }, index) => {
+                            const isLast = index === notices.length - 1;
+                            const classes = isLast ? 'p-4' : 'p-4 border-b border-blue-gray-50';
+
+                            return (
+                                <tr key={no}>
+                                    <td className={classes}>
+                                        <div className="flex items-center gap-3">
                                             <Typography
                                                 variant="small"
                                                 color="blue-gray"
-                                                className="font-normal"
+                                                className="font-bold"
                                             >
-                                                {title}
+                                                {no}
                                             </Typography>
-                                        </td>
-                                        <td className={classes}>
-                                            <Typography
-                                                variant="small"
-                                                color="blue-gray"
-                                                className="font-normal"
-                                            >
-                                                {date}
-                                            </Typography>
-                                        </td>
-                                    </tr>
-                                );
-                            })}
-                    </tbody>
-                </table>
-            </CardBody>
-            <CardFooter className="flex items-center justify-between border-t border-blue-gray-50 p-4">
-                <Button variant="outlined" size="sm">
-                    Previous
-                </Button>
-                <div className="flex items-center gap-2">
-                    <IconButton variant="outlined" size="sm">
-                        1
-                    </IconButton>
-                    <IconButton variant="text" size="sm">
-                        2
-                    </IconButton>
-                    <IconButton variant="text" size="sm">
-                        3
-                    </IconButton>
-                    <IconButton variant="text" size="sm">
-                        ...
-                    </IconButton>
-                    <IconButton variant="text" size="sm">
-                        8
-                    </IconButton>
-                    <IconButton variant="text" size="sm">
-                        9
-                    </IconButton>
-                    <IconButton variant="text" size="sm">
-                        10
-                    </IconButton>
-                </div>
-                <Button variant="outlined" size="sm">
-                    Next
-                </Button>
-            </CardFooter>
+                                        </div>
+                                    </td>
+                                    <td className={classes}>
+                                        <Typography
+                                            variant="small"
+                                            color="blue-gray"
+                                            className="font-normal"
+                                        >
+                                            {title}
+                                        </Typography>
+                                    </td>
+                                    <td className={classes}>
+                                        <Typography
+                                            variant="small"
+                                            color="blue-gray"
+                                            className="font-normal"
+                                        >
+                                            {date}
+                                        </Typography>
+                                    </td>
+                                </tr>
+                            );
+                        })}
+                </tbody>
+            </table>
         </>
     );
 }
