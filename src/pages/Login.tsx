667619729179be7d7bbf6fd400693fac1007f4ed
leekeunha <leetoya@naver.com>
2024-05-23 21:04:15 +0900

    css fix

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -4,10 +4,11 @@ import Logo from '../ui/Logo';
 
 export default function Login() {
     return (
-        <main className="min-h-screen flex flex-col gap-8">
-            <div className="flex justify-center">
+        <main className="flex flex-col items-center m-40">
+            <div className="flex justify-center mt-4">
                 <Logo className="h-24" />
             </div>
+
             <LoginForm />
         </main>
     );
