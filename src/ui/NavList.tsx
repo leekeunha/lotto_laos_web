b87fae80e51196b24a4c400cbe8bb018cd31d853
leekeunha <leetoya@naver.com>
2024-05-24 10:37:20 +0900

    gap-4

--- src/ui/NavList.tsx
+++ src/ui/NavList.tsx
@@ -2,7 +2,7 @@ import NavListMenus from './NavListMenus';
 
 export default function NavList() {
     return (
-        <ul className="mt-2 mb-4 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center">
+        <ul className="mt-2 mb-4 flex flex-col gap-4 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center">
             <NavListMenus />
         </ul>
     );
